0a79eeb06ef93a9bc8e351aa71442c1d2f40a6ec
lexa <sokolov@ozero.net>
2018-04-25 00:10:39 +0300

    Method implemented

--- src/main/java/LevelOrderTraversal.java
+++ src/main/java/LevelOrderTraversal.java
@@ -6,8 +6,20 @@ public class LevelOrderTraversal {
 
     }
 
-    public static Node insert() {
-        return null;
+    public static Node insert(Node root, int data) {
+        if (root == null) {
+            return new Node(data);
+        } else {
+            Node cur;
+            if (data <= root.data) {
+                cur = insert(root.left, data);
+                root.left = cur;
+            } else {
+                cur = insert(root.right, data);
+                root.right = cur;
+            }
+            return root;
+        }
     }
 }
 
