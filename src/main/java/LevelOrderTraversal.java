14e6532fdc1b4218879bd79d1a5c24d781d1379e
lexa <sokolov@ozero.net>
2018-04-25 00:06:38 +0300

    Class created, methods declared

--- src/main/java/LevelOrderTraversal.java
+++ src/main/java/LevelOrderTraversal.java
@@ -1,5 +1,24 @@
 public class LevelOrderTraversal {
     public static void main(String[] args) {
+    }
 
+    public static void levelOrder() {
+
+    }
+
+    public static Node insert() {
+        return null;
+    }
+}
+
+class Node {
+    Node left;
+    Node right;
+    int data;
+
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
     }
 }
